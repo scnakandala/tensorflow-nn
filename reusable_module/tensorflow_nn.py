0dbb1816123b1ead0fa70b00e6c87d927050edef
scnakandala <supun.nakandala@gmail.com>
2017-07-26 14:14:23 -0400

    wip

--- reusable_module/tensorflow_nn.py
+++ reusable_module/tensorflow_nn.py
@@ -12,7 +12,7 @@ class BNNState(object):
 class TFBNN(object):
     
     def __init__(self, num_neurons, iterations, h, firing_threshold, firing_reset,
-                 current_functions, initial_state, inputs, connections, connection_weights):
+                 current_func, initial_state, inputs, connections, connection_weights):
         self.num_neurons = num_neurons
         self.iterations = iterations
         
@@ -20,7 +20,7 @@ class TFBNN(object):
         self.firing_threshold = firing_threshold
         self.firing_reset = firing_reset
         
-        self.current_functions = current_functions
+        self.current_func = current_func
         self.initial_state = initial_state
         self.inputs = inputs
         
@@ -39,67 +39,68 @@ class TFBNN(object):
             return iteration_vals
     
     def __neuron_combined_steps(self, state, step_input):
+
+        [rk_variables, iteration, fired, fired_iteration] = state
+        
         with tf.name_scope('RK1'):
-            rk1 = self.__call_currents(state[0], state[1], state[2], state[3], step_input)
+            rk1 = self.__call_currents(rk_variables, iteration, fired, fired_iteration, step_input)
             
         with tf.name_scope('RK2'):
             rk2_in = []
             for i in range(len(rk1)):
-                rk2_in.append(tf.add(tf.multiply(tf.divide(self.h, tf.constant(2.)), rk1[i]), state[0][i]))
-                
-            iteration_rk2 = tf.add(state[2], tf.divide(self.h, tf.constant(2.)))
-            rk2 = self.__call_currents(rk2_in, iteration_rk2, state[2], state[3], step_input)
-            
+                temp = []
+                for j in range(len(rk1[i])):
+                    temp.append(tf.add(tf.multiply(tf.divide(self.h, tf.constant(2.)), rk1[i][j]), rk_variables[i][j]))
+                rk2_in.append(temp)
+            iteration_rk2 = tf.add(iteration, tf.divide(self.h, tf.constant(2.)))
+            rk2 = self.__call_currents(rk2_in, iteration_rk2, fired, fired_iteration, step_input)
+
         with tf.name_scope('RK3'):
             rk3_in = []
             for i in range(len(rk2)):
-                rk3_in.append(tf.add(tf.multiply(tf.divide(self.h, tf.constant(2.)), rk2[i]), state[0][i]))
-                
-            iteration_rk3 = tf.add(state[2], tf.divide(self.h, tf.constant(2.)))
-            rk3 = self.__call_currents(rk3_in, iteration_rk3, state[2], state[3], step_input)
+                temp = []
+                for j in range(len(rk2[i])):
+                    temp.append(tf.add(tf.multiply(tf.divide(self.h, tf.constant(2.)), rk2[i][j]), rk_variables[i][j]))
+                rk3_in.append(temp)
+            iteration_rk3 = tf.add(iteration, tf.divide(self.h, tf.constant(2.)))
+            rk3 = self.__call_currents(rk3_in, iteration_rk3, fired, fired_iteration, step_input)
             
         with tf.name_scope('RK4'):
             rk4_in = []
             for i in range(len(rk3)):
-                rk4_in.append(tf.add(tf.multiply(self.h, rk3[i]), state[0][i]))
-                
-            iteration_rk4 = tf.add(state[2], self.h)
-            rk4 = self.__call_currents(rk4_in, iteration_rk4, state[2], state[3], step_input)
-            
+                temp = []
+                for j in range(len(rk3[i])):
+                    temp.append(tf.add(tf.multiply(self.h, rk3[i][j]), rk_variables[i][j]))
+                rk4_in.append(temp)                
+            iteration_rk4 = tf.add(iteration, self.h)
+            rk4 = self.__call_currents(rk4_in, iteration_rk4, fired, fired_iteration, step_input)
+        
         rk_next = []
         for i in range(len(rk4)):
-            rk_next.append(tf.add(tf.multiply(tf.constant(1/6), tf.add_n([rk1[i], tf.multiply(tf.constant(2.),
-                        rk2[i]),tf.multiply(tf.constant(2.), rk3[i]), rk4[i]])), state[0][i]))
+            temp = []
+            for j in range(len(rk4[i])):
+                temp.append(
+                    tf.add(tf.multiply(tf.constant(1/6), tf.add_n([rk1[i][j], tf.multiply(tf.constant(2.),
+                        rk2[i][j]),tf.multiply(tf.constant(2.), rk3[i][j]), rk4[i][j]])), rk_variables[i][j]))
+            rk_next.append(temp)
             
-        fired = tf.where(tf.greater(rk_next[0], self.firing_threshold), tf.ones(self.num_neurons), tf.zeros(self.num_neurons))
-        state[3] = tf.where(tf.greater(rk_next[0], self.firing_threshold), tf.add(tf.constant(1.), state[3]), state[3])
+        fired_new = []
+        fired_iteration_new = []
         
-        # peak thresholding for neuron outputs
-        rk_next[0] = tf.where(tf.greater(rk_next[0], self.firing_threshold), self.firing_reset*tf.ones_like(rk_next[0]),
-                              rk_next[0])
+        for i in range(len(rk_next)):
+            fired_new.append(tf.where(tf.greater(rk_next[i][0], self.firing_threshold),
+                                  tf.ones_like(rk_next[i][0]), tf.zeros_like(rk_next[i][0])))
+            
+            fired_iteration_new.append(tf.where(tf.greater(rk_next[i][0], self.firing_threshold),
+                                            tf.add(tf.constant(1.), fired_iteration[i]), fired_iteration[i]))
 
-        state[0] = rk_next
-        state[1] = tf.add(state[1], 1.)
-        state[2] = fired
-
-        return state
+            # peak thresholding for neuron outputs
+            rk_next[i][0] = tf.where(tf.greater(rk_next[i][0], self.firing_threshold),
+                                     self.firing_reset*tf.ones_like(rk_next[i][0]), rk_next[i][0])
+        
+        return [rk_next, tf.add(state[1], 1.), fired_new, fired_iteration_new]
             
         
     def __call_currents(self, rk_variables, iteration, fired, fired_iteration, step_input):
-        with tf.name_scope('call_currents'):
-            current_aggregate = None
-            for fn in self.current_functions:
-                if current_aggregate is None:
-                    current_aggregate = fn(rk_variables, iteration, fired, fired_iteration, step_input,
+        return self.current_func(rk_variables, iteration, fired, fired_iteration, step_input,
                                   self.connections, self.connection_weights)
-                    for i in range(len(current_aggregate)):
-                        if current_aggregate[i] is None:
-                            current_aggregate[i] = tf.zeros(self.num_neurons)
-                else:
-                    rk_temp = fn(rk_variables, iteration, fired, fired_iteration, step_input,
-                                  self.connections, self.connection_weights)
-                    for i in range(len(rk_temp)):
-                        if rk_temp[i] is not None:
-                            current_aggregate[i] = tf.add(current_aggregate[i],rk_temp[i])
-                        
-        return current_aggregate
